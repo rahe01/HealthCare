18d011d602e7eb7aa76b7cd2c81a41f7e5fac27c
Rahe <130391604+rahe01@users.noreply.github.com>
2024-07-03 14:12:23 +0600

    first commit

---
+++ Client/src/Layout/Main.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Page/Home/Navbar/Navbar";
+
+
+const Main = () => {
+    return (
+        <div className="max-w-7xl mx-auto">
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Main;
